d674469c17bd2ec439668e7e1bd741c8643e42ba
Maik Macho <nuda1998@gmail.com>
2020-01-10 00:48:32 +0100

    Add a very basic map swap trigger

--- CollabModule.cs
+++ CollabModule.cs
@@ -1,8 +1,6 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using Celeste.Mod.CollabUtils2.Triggers;
+using Microsoft.Xna.Framework;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace Celeste.Mod.CollabUtils2 {
     public class CollabModule : EverestModule {
@@ -14,9 +12,30 @@ namespace Celeste.Mod.CollabUtils2 {
         }
 
         public override void Load() {
+            Everest.Events.Level.OnLoadEntity += OnLoadEntity;
         }
 
         public override void Unload() {
+            Everest.Events.Level.OnLoadEntity -= OnLoadEntity;
         }
+
+        private static bool OnLoadEntity(Level level, LevelData levelData, Vector2 offset, EntityData entityData) {
+            // Allow using the Everest flag trigger with custom "commands" because I don't have time for Ahorn plugins. -ade
+            if (entityData.Name == "everest/flagTrigger" && entityData.Attr("flag").StartsWith("/collab2:")) {
+                string[] args = entityData.Attr("flag").Substring(9).Split(' ');
+                switch (args[0]) {
+                    case "mapswap":
+                        level.Add(new MapSwapTrigger(entityData, offset) {
+                            map = args.ElementAtOrDefault(1),
+                            side = args.ElementAtOrDefault(2),
+                            room = args.ElementAtOrDefault(3)
+                        });
+                        return true;
+                }
+            }
+
+            return false;
+        }
+
     }
 }
---
+++ Triggers/MapSwapTrigger.cs
@@ -0,0 +1,41 @@
+﻿using Celeste.Mod.Entities;
+using Microsoft.Xna.Framework;
+using System;
+
+namespace Celeste.Mod.CollabUtils2.Triggers {
+    [CustomEntity("CollabUtils2/MapSwapTrigger")]
+    public class MapSwapTrigger : Trigger {
+
+        public string map;
+        public string side;
+        public string room;
+
+        private bool swapping;
+
+        public MapSwapTrigger(EntityData data, Vector2 offset)
+            : base(data, offset) {
+            map = data.Attr("map");
+            side = data.Attr("side");
+            room = data.Attr("room");
+        }
+
+        public override void OnEnter(Player player) {
+            base.OnEnter(player);
+
+            if (swapping)
+                return;
+            swapping = true;
+
+            Level level = SceneAs<Level>();
+
+            SceneAs<Level>().DoScreenWipe(false, () => {
+                if (string.IsNullOrEmpty(room) || room == "-")
+                    room = null;
+                if (!Enum.TryParse(side, out AreaMode mode))
+                    mode = AreaMode.Normal;
+                LevelEnter.Go(new Session(AreaData.Get(map)?.ToKey(mode) ?? new AreaKey(-1), room), false);
+            });
+        }
+
+    }
+}
