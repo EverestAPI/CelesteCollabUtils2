dab84359f9ef06195e3ff80222d59a6625427ac2
max480 <max480@free.fr>
2020-05-11 14:39:40 +0200

    PERFECT animation when collecting rainbow berry

---
+++ Entities/RainbowBerryPerfectEffect.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.Xna.Framework;
+using Monocle;
+
+namespace Celeste.Mod.CollabUtils2.Entities {
+    class RainbowBerryPerfectEffect : Entity {
+
+        public RainbowBerryPerfectEffect(Vector2 position)
+            : base(position) {
+
+            Sprite sprite;
+            Depth = -1000000;
+            Add(sprite = RainbowBerry.SpriteBank.Create("perfectAnimation"));
+            sprite.OnLastFrame = delegate {
+                RemoveSelf();
+            };
+            sprite.Play("perfect");
+        }
+    }
+}
