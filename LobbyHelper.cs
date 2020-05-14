3875828831d69a48e4204f8c5c7dc8fcf32e9632
max480 <max480@free.fr>
2020-05-15 01:39:19 +0200

    Hide collab entries from chapter select

--- LobbyHelper.cs
+++ LobbyHelper.cs
@@ -1,5 +1,11 @@
-﻿using MonoMod.Utils;
+﻿using Celeste.Mod.UI;
+using Mono.Cecil;
+using Mono.Cecil.Cil;
+using MonoMod.Cil;
+using MonoMod.RuntimeDetour;
+using MonoMod.Utils;
 using System;
+using System.Collections;
 using System.Linq;
 
 namespace Celeste.Mod.CollabUtils2 {
@@ -19,14 +25,35 @@ namespace Celeste.Mod.CollabUtils2 {
             return null;
         }
 
+        /// <summary>
+        /// Returns true if the given level set is a hidden level set from a collab.
+        /// </summary>
+        public static bool IsCollabLevelSet(string levelSet) {
+            return levelSet.StartsWith("SpringCollab2020/") && levelSet != "SpringCollab2020/0-Lobbies";
+        }
+
+        private static ILHook hookOnLevelSetSwitch;
+
         public static void Load() {
+            // timer pausing when returning to lobby
             On.Celeste.Level.LoadLevel += onLoadLevel;
             On.Celeste.Player.Update += onPlayerUpdate;
+
+            // hiding collab maps from chapter select
+            hookOnLevelSetSwitch = HookHelper.HookCoroutine("Celeste.Mod.UI.OuiHelper_ChapterSelect_LevelSet", "Enter", modLevelSetSwitch);
+            IL.Celeste.Mod.UI.OuiMapSearch.ReloadItems += modMapSearch;
+            IL.Celeste.Mod.UI.OuiMapList.ReloadItems += modMapListReloadItems;
+            IL.Celeste.Mod.UI.OuiMapList.CreateMenu += modMapListCreateMenu;
         }
 
         public static void Unload() {
             On.Celeste.Level.LoadLevel -= onLoadLevel;
             On.Celeste.Player.Update -= onPlayerUpdate;
+
+            hookOnLevelSetSwitch?.Dispose();
+            IL.Celeste.Mod.UI.OuiMapSearch.ReloadItems -= modMapSearch;
+            IL.Celeste.Mod.UI.OuiMapList.ReloadItems -= modMapListReloadItems;
+            IL.Celeste.Mod.UI.OuiMapList.CreateMenu -= modMapListCreateMenu;
         }
 
         public static void OnSessionCreated() {
@@ -63,6 +90,86 @@ namespace Celeste.Mod.CollabUtils2 {
 
                 self.SceneAs<Level>().TimerStopped = false;
                 unpauseTimerOnNextAction = false;
+            }
+        }
+
+        private static void modLevelSetSwitch(ILContext il) {
+            ILCursor cursor = new ILCursor(il);
+
+            // target check: areaData.GetLevelSet() != levelSet
+            if (cursor.TryGotoNext(MoveType.After,
+                instr => instr.MatchLdloc(6), // AreaData getting considered
+                instr => instr.MatchCall(typeof(AreaDataExt), "GetLevelSet"),
+                instr => instr.MatchLdloc(3), // current level set
+                instr => instr.MatchCall<string>("op_Inequality"))) {
+
+                Logger.Log("CollabUtils2/LobbyHelper", $"Making chapter select skip hidden collab level sets at {cursor.Index} in IL for OuiHelper_ChapterSelect_LevelSet.Enter");
+
+                // becomes: areaData.GetLevelSet() != levelSet && !IsCollabLevelSet(areaData.GetLevelSet())
+                cursor.Emit(OpCodes.Ldloc_S, (byte) 6);
+                cursor.EmitDelegate<Func<bool, AreaData, bool>>((orig, areaData) =>
+                    orig && !IsCollabLevelSet(areaData.GetLevelSet()));
+            }
+        }
+
+        private static void modMapSearch(ILContext il) {
+            ILCursor cursor = new ILCursor(il);
+
+            // target check: area.HasMode(AreaMode.Normal)
+            // area is actually stored in a "DisplayClass" nested type, explaining the extra ldfld "area".
+            if (cursor.TryGotoNext(MoveType.After,
+                instr => instr.MatchLdloc(13),
+                instr => instr.OpCode == OpCodes.Ldfld && (instr.Operand as FieldReference).Name == "area",
+                instr => instr.MatchLdcI4(0),
+                instr => instr.MatchCallvirt<AreaData>("HasMode"))) {
+
+                Logger.Log("CollabUtils2/LobbyHelper", $"Hiding collab entries in map search at {cursor.Index} in IL for OuiMapSearch.ReloadItems");
+
+                // becomes: area.HasMode(AreaMode.Normal) && !IsCollabLevelSet(area.GetLevelSet())
+                cursor.Emit(OpCodes.Ldloc_S, (byte) 13);
+                cursor.Emit(OpCodes.Ldfld, cursor.Instrs[cursor.Index - 4].Operand as FieldReference);
+                cursor.EmitDelegate<Func<bool, AreaData, bool>>((orig, areaData) =>
+                    orig && !IsCollabLevelSet(areaData.GetLevelSet()));
+            }
+        }
+
+        private static void modMapListReloadItems(ILContext il) {
+            ILCursor cursor = new ILCursor(il);
+
+            // target check: area.HasMode((AreaMode)side)
+            // area is actually stored in a "DisplayClass" nested type, explaining the extra ldfld "area".
+            if (cursor.TryGotoNext(MoveType.After,
+                instr => instr.MatchLdloc(12),
+                instr => instr.OpCode == OpCodes.Ldfld && (instr.Operand as FieldReference).Name == "area",
+                instr => instr.MatchLdarg(0),
+                instr => instr.MatchLdfld<OuiMapList>("side"),
+                instr => instr.MatchCallvirt<AreaData>("HasMode"))) {
+
+                Logger.Log("CollabUtils2/LobbyHelper", $"Hiding collab entries from map list at {cursor.Index} in IL for OuiMapList.ReloadItems");
+
+                // becomes: area.HasMode((AreaMode)side) && !IsCollabLevelSet(area.GetLevelSet())
+                cursor.Emit(OpCodes.Ldloc_S, (byte) 12);
+                cursor.Emit(OpCodes.Ldfld, cursor.Instrs[cursor.Index - 5].Operand as FieldReference);
+                cursor.EmitDelegate<Func<bool, AreaData, bool>>((orig, areaData) =>
+                    orig && !IsCollabLevelSet(areaData.GetLevelSet()));
+            }
+        }
+
+        private static void modMapListCreateMenu(ILContext il) {
+            ILCursor cursor = new ILCursor(il);
+
+            // target check: levelSet == "Celeste"
+            if (cursor.TryGotoNext(MoveType.After,
+                instr => instr.MatchLdloc(1),
+                instr => instr.MatchLdstr("Celeste"),
+                instr => instr.MatchCall<string>("op_Equality"))) {
+
+                Logger.Log("CollabUtils2/LobbyHelper", $"Hiding collab entries in level set list at {cursor.Index} in IL for OuiMapList.ReloadItems");
+
+                // becomes: levelSet == "Celeste" || IsCollabLevelSet(levelSet)
+                cursor.Emit(OpCodes.Ldloc_1);
+                cursor.EmitDelegate<Func<bool, string, bool>>((orig, levelSet) =>
+                    orig || IsCollabLevelSet(levelSet));
             }
         }
     }
