0a55b1d42efb14e571e7e09034994be11fde153e
max4805 <max480@free.fr>
2020-06-02 00:13:27 +0200

    Add rainbow berry hologram showing silver berry progress

--- Entities/RainbowBerry.cs
+++ Entities/RainbowBerry.cs
@@ -30,14 +30,23 @@ namespace Celeste.Mod.CollabUtils2.Entities {
             new DynData<Strawberry>(this).Get<BloomPoint>("bloom").Alpha = 0.5f;
 
             if (CollabMapDataProcessor.SilverBerries.ContainsKey(levelSet)) {
+                int missingBerries = 0;
+                int totalBerries = CollabMapDataProcessor.SilverBerries[levelSet].Count;
                 foreach (KeyValuePair<string, EntityID> requiredSilver in CollabMapDataProcessor.SilverBerries[levelSet]) {
                     // check if the silver was collected.
                     AreaStats stats = SaveData.Instance.GetAreaStatsFor(AreaData.Get(requiredSilver.Key).ToKey());
                     if (!stats.Modes[0].Strawberries.Contains(requiredSilver.Value)) {
-                        // this berry wasn't collected, so the rainbow shouldn't be here! kick it out.
-                        RemoveSelf();
-                        return;
+                        // this berry wasn't collected!
+                        missingBerries++;
                     }
+                }
+
+                if (missingBerries != 0) {
+                    // some berries are missing, spawn the hologram instead of the berry.
+                    HoloRainbowBerry hologram = new HoloRainbowBerry(Position, totalBerries - missingBerries, totalBerries);
+                    scene.Add(hologram);
+
+                    RemoveSelf();
                 }
             }
         }
