18f3903c6cfbc3afc04d09a71766bdcf1699788a
max4805 <max480@free.fr>
2020-08-10 22:04:28 +0200

    Golden Berry Player Respawn Point: respawn point for silver/speed/golden berry resets

---
+++ Ahorn/entities/goldenBerrySpawnPoint.jl
@@ -0,0 +1,24 @@
+﻿module CollabUtils2GoldenBerryPlayerRespawnPoint
+
+using ..Ahorn, Maple
+
+@mapdef Entity "CollabUtils2/GoldenBerryPlayerRespawnPoint" GoldenBerryPlayerRespawnPoint(x::Integer, y::Integer)
+
+const placements = Ahorn.PlacementDict(
+    "Golden Berry Player Respawn Point (Collab Utils 2)" => Ahorn.EntityPlacement(
+        GoldenBerryPlayerRespawnPoint
+    )
+)
+
+const sprite = "characters/player/sitDown00"
+const tint = (0.8, 0.6, 0.1, 0.75)
+
+function Ahorn.selection(entity::GoldenBerryPlayerRespawnPoint)
+    x, y = Ahorn.position(entity)
+
+    return Ahorn.getSpriteRectangle(sprite, x, y, jx=0.5, jy=1.0)
+end
+
+Ahorn.render(ctx::Ahorn.Cairo.CairoContext, entity::GoldenBerryPlayerRespawnPoint) = Ahorn.drawSprite(ctx, sprite, 0, 0, jx=0.5, jy=1.0, tint=tint)
+
+end
