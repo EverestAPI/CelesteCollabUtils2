d811dd426e6b84579cc0c93044294323493fc435
Shane Woolcock <samahnub@gmail.com>
2023-01-28 18:07:24 +1030

    Add activation animation to warps

--- Entities/LobbyMapWarp.cs
+++ Entities/LobbyMapWarp.cs
@@ -2,6 +2,7 @@ using Celeste.Mod.CollabUtils2.UI;
 using Celeste.Mod.Entities;
 using Microsoft.Xna.Framework;
 using Monocle;
+using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
 
@@ -12,16 +13,24 @@ namespace Celeste.Mod.CollabUtils2.Entities {
 
         private readonly string spriteName;
         private readonly string spriteAnimation;
+        private readonly string activateSpriteName;
+        private readonly string activateSpriteAnimation;
+        private readonly Facings playerFacing;
+        
         private readonly Sprite sprite;
         private LobbyMapController.FeatureInfo info;
 
         public LobbyMapWarp(EntityData data, Vector2 offset) : base(data.Position + offset) {
             spriteName = data.Attr("spriteName");
             spriteAnimation = data.Attr("spriteAnimation");
+            activateSpriteName = data.Attr("activateSpriteName");
+            playerFacing = (Facings) data.Int("playerFacing", (int) Facings.Right);
 
             LobbyMapController.FeatureInfo.TryParse(data, default, out info);
             
-            Add(new TalkComponent(new Rectangle(-16, -32, 32, 32), Vector2.Zero, onTalk));
+            Add(new TalkComponent(new Rectangle(-16, -32, 32, 32), Vector2.Zero, onTalk) {
+                PlayerMustBeFacing = false,
+            });
 
             if (!string.IsNullOrWhiteSpace(spriteName)) {
                 Add(sprite = GFX.SpriteBank.Create(spriteName));
@@ -48,9 +57,52 @@ namespace Celeste.Mod.CollabUtils2.Entities {
                 if (!warps.Contains(info.FeatureId)) {
                     warps.Add(info.FeatureId);
                 }
-                
-                level.Add(new LobbyMapUI());
+
+                if (!string.IsNullOrWhiteSpace(activateSpriteName)) {
+                    Add(new Coroutine(activateRoutine(player)));
+                } else {
+                    level.Add(new LobbyMapUI());
+                }
             }
+        }
+
+        private IEnumerator activateRoutine(Player player) {
+            if (player == null) yield break;
+
+            player.StateMachine.State = Player.StDummy;
+            yield return player.DummyWalkToExact((int)X, false, 1f, true);
+            player.Facing = playerFacing;
+
+            player.Sprite.Visible = player.Hair.Visible = false;
+
+            var playerSprite = new Sprite(GFX.Game, activateSpriteName);
+            playerSprite.Add("idle", "", 0.08f);
+            playerSprite.CenterOrigin();
+            playerSprite.Position += new Vector2(0, -16);
+            Add(playerSprite);
+
+            var playerHairSprite = new Sprite(GFX.Game, activateSpriteName + "Hair");
+            playerHairSprite.Add("idle", "", 0.08f);
+            playerHairSprite.CenterOrigin();
+            playerHairSprite.Position += new Vector2(0, -16);
+            playerHairSprite.Color = player.Hair.Color;
+            Add(playerHairSprite);
+            
+            playerSprite.Play("idle");
+            playerHairSprite.Play("idle");
+
+            while (playerSprite.Animating) {
+                yield return null;
+            }
+            
+            player.Scene.Add(new LobbyMapUI());
+
+            yield return 0.1f;
+            
+            playerSprite.RemoveSelf();
+            playerHairSprite.RemoveSelf();
+            
+            player.Sprite.Visible = player.Hair.Visible = true;
         }
     }
 }
