c158b0645d8f5448f0053096acc018f8bb01acb5
Shane Woolcock <samahnub@gmail.com>
2024-01-02 12:21:52 +1030

    Add default lobby map keybinds

--- CollabSettings.cs
+++ CollabSettings.cs
@@ -1,14 +1,27 @@
 
+using Microsoft.Xna.Framework.Input;
+
 namespace Celeste.Mod.CollabUtils2 {
     public class CollabSettings : EverestModuleSettings {
         public enum SpeedBerryTimerPositions { TopLeft, TopCenter };
         public enum BestTimeInJournal { SpeedBerry, ChapterTimer };
 
+        [DefaultButtonBinding(Buttons.RightStick, Keys.Tab)]
         public ButtonBinding DisplayLobbyMap { get; set; }
+
+        [DefaultButtonBinding(0, Keys.LeftShift)]
         public ButtonBinding HoldToPan { get; set; }
+
+        [DefaultButtonBinding(Buttons.RightThumbstickUp, 0)]
         public ButtonBinding PanLobbyMapUp { get; set; }
+
+        [DefaultButtonBinding(Buttons.RightThumbstickDown, 0)]
         public ButtonBinding PanLobbyMapDown { get; set; }
+
+        [DefaultButtonBinding(Buttons.RightThumbstickLeft, 0)]
         public ButtonBinding PanLobbyMapLeft { get; set; }
+
+        [DefaultButtonBinding(Buttons.RightThumbstickRight, 0)]
         public ButtonBinding PanLobbyMapRight { get; set; }
 
         public SpeedBerryTimerPositions SpeedBerryTimerPosition { get; set; } = SpeedBerryTimerPositions.TopLeft;
